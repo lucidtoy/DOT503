9321fe86bec35d4a74473052ba30b46f2b3d9308
Solomon Oladokun <lucidtoy@yahoo.com>
2023-11-14 13:28:20 +1030

    Changes for feature-x

---
+++ JunitTestCaseExampleX.java
@@ -0,0 +1,29 @@
+package JavaTpoint. JunitExamples;  
+import java.util.ArrayList;  
+import java.util.List;  
+public class JunitTestCaseExample {  
+    private List<String> students = new ArrayList<String>();  
+  
+    public void remove(String name) {  
+        students.remove(name);  
+    }
+      
+    public void add(String name) {  
+        students.add(name);  
+    }
+
+    #modification for feature_x
+    public void add_x(String name) {  
+        students.add(name);  
+    }
+
+      
+    public void removeAll(){  
+        students.clear();  
+    }
+      
+    public int sizeOfStudent() {  
+        return students.size();  
+    }  
+      
+}
