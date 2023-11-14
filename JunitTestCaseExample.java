743eece86e77837a6b77baecb45f872510bdd8c6
Solomon Oladokun <lucidtoy@yahoo.com>
2023-11-14 13:12:37 +1030

    Changes for feature-x

--- JunitTestCaseExample.java
+++ JunitTestCaseExample.java
@@ -6,15 +6,21 @@ public class JunitTestCaseExample {
   
     public void remove(String name) {  
         students.remove(name);  
-    }  
+    }
       
     public void add(String name) {  
         students.add(name);  
-    }  
+    }
+
+    #modification for feature_x
+    public void add_x(String name) {  
+        students.add(name);  
+    }
+
       
     public void removeAll(){  
         students.clear();  
-    }  
+    }
       
     public int sizeOfStudent() {  
         return students.size();  
