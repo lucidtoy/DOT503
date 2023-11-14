84a068a193a8e2369da9d5c9d20cdae5c8974beb
Solomon Oladokun <lucidtoy@yahoo.com>
2023-11-15 09:57:05 +1030

    Feature-x latest modification

--- JunitTestCaseExample.java
+++ JunitTestCaseExample.java
@@ -3,22 +3,28 @@ import java.util.ArrayList;
 import java.util.List;  
 public class JunitTestCaseExample {  
     private List<String> students = new ArrayList<String>();  
+    private List<String> students2 = new ArrayList<String>();  
   
     public void remove(String name) {  
         students.remove(name);  
-    }  
+    }
       
     public void add(String name) {  
-        students.add(name);  
+        students.add(name2);  
     }  
-    //feature for x
+  
+    //feature for x and Z merged
     public void add_x(String name) {  
         students.add(name);  
-    }  
-    
+    }
+
+    //New feature for x
+    public void add_xx(String name) {  
+        students.add(name);  
+    }
     public void removeAll(){  
         students.clear();  
-    }  
+    }
       
     public int sizeOfStudent() {  
         return students.size();  
