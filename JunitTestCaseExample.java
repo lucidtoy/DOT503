3a7870ef7a0546bd9578ac571fcd1edd2a3e33f7
Solomon Oladokun <lucidtoy@yahoo.com>
2023-11-14 13:49:05 +1030

    Update JunitTestCaseExample.java

--- JunitTestCaseExample.java
+++ JunitTestCaseExample.java
@@ -3,16 +3,20 @@ import java.util.ArrayList;
 import java.util.List;  
 public class JunitTestCaseExample {  
     private List<String> students = new ArrayList<String>();  
+    private List<String> students2 = new ArrayList<String>();  
   
     public void remove(String name) {  
         students.remove(name);  
     }  
-    
-     //feature for x
-    public void add_x(String name) {  
-        students.add();  
-    }  
       
+    public void add(String name) {  
+        students.add(name2);  
+    }  
+    //feature for y
+    public void add_x(String name2) {  
+        students.add(name2);  
+    }  
+    
     public void removeAll(){  
         students.clear();  
     }  
